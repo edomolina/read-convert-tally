89c4591b4b0c869844874a22193e954c71b57fc6
Edo Molina <edo.molina@mailersplus.com>
2015-04-16 23:10:01 -0400

    Write Two Tabs

--- FileReader.js
+++ FileReader.js
@@ -1,13 +1,11 @@
 var fs = require('fs');
-
 var XLSX = require('XLSX');
-
 var _ = require('underscore');
-
 var watch = require ('watch');
 
 var patternList = {};
-
+var statecount = {};
+var tabs = ["MKTID","STATE COUNT"];
 
 //dont know why
 var wscols = [
@@ -54,14 +52,8 @@ watch.createMonitor('/users/molinae/dev/read-convert-tally/', function (monitor)
 				}
 
 				//create excel file
+				var index;
 
-				var ws_name = "MKTID";
-
-				var sheetdata = [];
-
-				sheetdata = _.pairs(patternList);
-
-				sheetdata.unshift("ID","COUNT");
 
 				function Workbook() {
 					if(!(this instanceof Workbook)) return new Workbook();
@@ -69,93 +61,112 @@ watch.createMonitor('/users/molinae/dev/read-convert-tally/', function (monitor)
 					this.Sheets = {};
 				}
 				var wb = new Workbook();
+				var sheetdata = [];
 
-				/* convert an array of arrays in JS to a CSF spreadsheet */
-				function sheet_from_array_of_arrays(sheetdata, opts) {
-					var ws = {};
-					var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
-					for(var R = 0; R != sheetdata.length; ++R) {
-						for(var C = 0; C != sheetdata[R].length; ++C) {
-							if(range.s.r > R) range.s.r = R;
-							if(range.s.c > C) range.s.c = C;
-							if(range.e.r < R) range.e.r = R;
-							if(range.e.c < C) range.e.c = C;
-							var cell = {v: sheetdata[R][C] };
+				for (index = 0; index < tabs.length; ++index) {
 
-							if(cell.v == null) continue;
-							var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
-
-							/* TEST: proper cell types and value handling */
-							if(typeof cell.v === 'number') cell.t = 'n';
-							else if(typeof cell.v === 'boolean') cell.t = 'b';
-							else if(cell.v instanceof Date) {
-								cell.t = 'n'; cell.z = XLSX.SSF._table[14];
-								cell.v = datenum(cell.v);
+					var ws_name = tabs[index];
+					switch (ws_name) {
+						case 'MKTID':
+							sheetdata = _.pairs(patternList);
+							break;
+							case 'STATE COUNT':
+								sheetdata = _.pairs(statecount);
+								break;
 							}
-							else cell.t = 's';
-							ws[cell_ref] = cell;
+
+
+							/* convert an array of arrays in JS to a CSF spreadsheet */
+							function sheet_from_array_of_arrays(sheetdata, opts) {
+								var ws = {};
+								var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+								for(var R = 0; R != sheetdata.length; ++R) {
+									for(var C = 0; C != sheetdata[R].length; ++C) {
+										if(range.s.r > R) range.s.r = R;
+										if(range.s.c > C) range.s.c = C;
+										if(range.e.r < R) range.e.r = R;
+										if(range.e.c < C) range.e.c = C;
+										var cell = {v: sheetdata[R][C] };
+
+										if(cell.v == null) continue;
+										var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
+
+										/* TEST: proper cell types and value handling */
+										if(typeof cell.v === 'number') cell.t = 'n';
+										else if(typeof cell.v === 'boolean') cell.t = 'b';
+										else if(cell.v instanceof Date) {
+											cell.t = 'n'; cell.z = XLSX.SSF._table[14];
+											cell.v = datenum(cell.v);
+										}
+										else cell.t = 's';
+										ws[cell_ref] = cell;
+									}
+								}
+
+								/* TEST: proper range */
+								if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+								return ws;
+							}
+							var ws = sheet_from_array_of_arrays(sheetdata);
+
+							wb.SheetNames.push(ws_name);
+							wb.Sheets[ws_name] = ws;
+
+							/* TEST: column widths */
+							ws['!cols'] = wscols;
+
+							/* write file */
+							XLSX.writeFile(wb, 'ClientReport.xlsx');
+							console.log('Excel File Created.');
 						}
+
+					});
+				}
+
+				var input = fs.createReadStream('ver.txt');
+				readLines(input, function(line) {
+					var fname=line.substr(0, 12);
+					var lname=line.substr(13, 16);
+					var add=line.substr(29, 24);
+					var city=line.substr(53, 18);
+					var state=line.substr(71, 12)
+					var zip=line.substr(83, 10);
+					var id=line.substr(93, 2);
+
+					if (patternList[id] === undefined) {
+						patternList[id] = 1;
+					} else {
+						patternList[id] += 1;
 					}
 
-					/* TEST: proper range */
-					if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
-					return ws;
-				}
-				var ws = sheet_from_array_of_arrays(sheetdata);
-
-				/* TEST: add worksheet to workbook */
-				wb.SheetNames.push(ws_name);
-				wb.Sheets[ws_name] = ws;
-
-				/* TEST: column widths */
-				ws['!cols'] = wscols;
-
-				/* write file */
-				XLSX.writeFile(wb, 'ClientReport.xlsx');
-				console.log('Excel File Created.');
+					if (statecount[state] === undefined) {
+						statecount[state] = 1;
+					} else {
+						statecount[state] += 1;
+					}
 
 
-			});
-		}
+					//  console.log(id, patternList[id]);
 
-		var input = fs.createReadStream('ver.txt');
-		readLines(input, function(line) {
-			var fname=line.substr(0, 12);
-			var lname=line.substr(13, 16);
-			var add=line.substr(29, 24);
-			var city=line.substr(53, 18);
-			var state=line.substr(71, 12)
-			var zip=line.substr(83, 10);
-			var id=line.substr(93, 2);
+					var newline=fname.trim() + '|' + lname.trim() + '|' + add.trim() + '|' + city.trim() + '|' + state.trim() + '|' + zip.trim() + '|' + id.trim() + '\n';
 
-			//id iteration object
-			if (patternList[id] === undefined) {
-				patternList[id] = 1;
-			} else {
-				patternList[id] += 1;
-			}
+					fs.appendFile('pipefile.txt', newline, function (err) {
+						if (err) throw err;
+					});
 
-			//  console.log(id, patternList[id]);
+				});
+				console.log('Fix Width File Converted to Pipe File.');
 
-			var newline=fname.trim() + '|' + lname.trim() + '|' + add.trim() + '|' + city.trim() + '|' + state.trim() + '|' + zip.trim() + '|' + id.trim() + '\n';
+			})
 
-			fs.appendFile('pipefile.txt', newline, function (err) {
-				if (err) throw err;
-			});
+			//monitor.stop();
+			//	monitor.on("changed", function (f, curr, prev) {
+			//		// Handle file changes
+			//	})
+			//	monitor.on("removed", function (f, stat) {
+			//		// Handle removed files
+			//	})
+			//	monitor.stop(); // Stop watching
+			//})
 
-		});
-		console.log('Fix Width File Converted to Pipe File.');
-
-	})
-
-	//monitor.stop();
-	//	monitor.on("changed", function (f, curr, prev) {
-	//		// Handle file changes
-	//	})
-	//	monitor.on("removed", function (f, stat) {
-	//		// Handle removed files
-	//	})
-	//	monitor.stop(); // Stop watching
-	//})
-
-})
+		})
